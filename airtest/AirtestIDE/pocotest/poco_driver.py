ede4e676050f0ac8f0404b16f0a9280806161b7a
xmznmn1983 <Simon@joycastle.mobi>
2023-10-31 18:24:42 +0800

    cli_setup

--- airtest/AirtestIDE/pocotest/poco_driver.py
+++ airtest/AirtestIDE/pocotest/poco_driver.py
@@ -5,10 +5,22 @@ import sys
 sys.path.append('.')
 from unittest.case import TestCase
 from poco.drivers.unity3d import UnityPoco
-
+from airtest.cli.parser import cli_setup
+from airtest.core.api import *
 
 class BingoVTestCase(TestCase):
     poco = None
+    def setUp(self):
+        self.log = "E:\\test\\testtools\\airtest"
+        self.airtest_path = "E:\\test\\testtools\\airtest\\log"
+        self.app_package = "com.bingo.cruise.free.best.top.game"
+        self.package_path = "D:/Downloads/BingoVoyage_dev_android_package_1613_pocotest_test_1.22.1.apk"
+        import pocotest.element_path
+        self.element_path = pocotest.element_path
+        self.android_device = "Android://127.0.0.1:5037/R58M66VY68Y"
+        if not cli_setup():
+            auto_setup(__file__, logdir=self.log,
+                       devices=[self.android_device], project_root=self.airtest_path)
 
     @classmethod
     def setUpClass(cls):
--- airtest/AirtestIDE/pocotest/testcase/rungame.py
+++ airtest/AirtestIDE/pocotest/testcase/rungame.py
@@ -1,7 +1,6 @@
 import sys
 sys.path.append("C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\_airtest\\airtest\\AirtestIDE\\pocotest")
 from poco_driver import BingoVTestCase
-from airtest.cli.parser import cli_setup
 from airtest.core.api import *
 from airtest.report.report import simple_report
 
@@ -9,32 +8,23 @@ from airtest.report.report import simple_report
 class Start(BingoVTestCase):
 
     def setUp(self):
-        self.log = "E:\\test\\testtools\\airtest"
-        self.airtest_path = "E:\\test\\testtools\\airtest\\log"
-        self.app_package = "com.bingo.cruise.free.best.top.game"
-        self.package_path = "D:/Downloads/BingoVoyage_dev_android_package_1613_pocotest_test_1.22.1.apk"
-        self.android_device = "Android://127.0.0.1:5037/R58M66VY68Y"
-        import pocotest.element_path
-        self.element_path = pocotest.element_path
         os.system("adb devices")
 
     def runGame(self):
-        if not cli_setup():
-            auto_setup(__file__, logdir=self.log,
-                       devices=[self.android_device], project_root=self.airtest_path)
-            uninstall(self.app_package)
-            time.sleep(1)
-            install(self.package_path)
-            time.sleep(1)
-            start_app(self.app_package)
-            time.sleep(10)
 
-            # print("start...")
-            import pocotest.action
-            pocotest.action.click_button(self.element_path.login)
-            time.sleep(1)
-            stop_app(self.app_package)
-            # generate html report
+        uninstall(self.app_package)
+        time.sleep(1)
+        install(self.package_path)
+        time.sleep(1)
+        start_app(self.app_package)
+        time.sleep(10)
+
+        # print("start...")
+        import pocotest.action
+        pocotest.action.click_button(self.element_path.login)
+        time.sleep(1)
+        stop_app(self.app_package)
+        # generate html report
 
     def runTest(self):
         pass
