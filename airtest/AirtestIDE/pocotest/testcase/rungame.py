107370edf9b057b81a21e03cda601d7aafc25a80
xmznmn1983 <Simon@joycastle.mobi>
2023-10-31 17:47:23 +0800

    init

--- airtest/AirtestIDE/pocotest/testcase/rungame.py
+++ airtest/AirtestIDE/pocotest/testcase/rungame.py
@@ -8,18 +8,20 @@ from airtest.report.report import simple_report
 
 class Start(BingoVTestCase):
 
-    def setUp(self):
+    def __init__(self):
         self.log = "E:\\test\\testtools\\airtest"
         self.airtest_path = "E:\\test\\testtools\\airtest\\log"
         self.app_package = "com.bingo.cruise.free.best.top.game"
         self.package_path = "D:/Downloads/BingoVoyage_dev_android_package_1613_pocotest_test_1.22.1.apk"
+        self.android_device = "Android://127.0.0.1:5037/R58M66VY68Y"
         import pocotest.element_path
         self.element_path = pocotest.element_path
         os.system("adb devices")
 
+    def setUp(self):
         if not cli_setup():
             auto_setup(__file__, logdir=self.log,
-                       devices=["Android://127.0.0.1:5037/emulator-5554"], project_root=self.airtest_path)
+                       devices=[self.android_device], project_root=self.airtest_path)
             uninstall(self.app_package)
             time.sleep(1)
             install(self.package_path)
@@ -39,6 +41,5 @@ class Start(BingoVTestCase):
 
     def tearDown(self):
         simple_report(__file__)
-
 
 
