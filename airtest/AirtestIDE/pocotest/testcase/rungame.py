8e459f06959290ad309baae772951c7a517612c1
xmznmn1983 <Simon@joycastle.mobi>
2023-10-31 19:33:35 +0800

    path

--- airtest/AirtestIDE/pocotest/testcase/rungame.py
+++ airtest/AirtestIDE/pocotest/testcase/rungame.py
@@ -13,8 +13,8 @@ class BingoVTestCase:
         os.system("adb devices")
         self.app_package = "com.bingo.cruise.free.best.top.game"
         self.package_path = "D:/Downloads/BingoVoyage_dev_android_package_1613_pocotest_test_1.22.1.apk"
-        import pocotest.element_path
-        self.element_path = pocotest.element_path
+        import element_path
+        self.element_path = element_path
         self.test_log = "E:\\test\\testtools\\airtest"
         self.log_path = "E:\\test\\testtools\\airtest\\log"
         self.test_device = "Android://127.0.0.1:5037/R58M66VY68Y"
@@ -31,8 +31,8 @@ class BingoVTestCase:
         start_app(self.app_package)
         time.sleep(10)
         # print("start...")
-        import pocotest.action
-        pocotest.action.click_button(self.element_path.login)
+        import action
+        action.click_button(self.element_path.login)
         time.sleep(1)
         stop_app(self.app_package)
         # generate html report
