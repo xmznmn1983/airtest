b8f1bc4807c7557bb21aa54da85df5074e0bf1d3
xmznmn1983 <Simon@joycastle.mobi>
2023-11-01 23:55:29 +0800

    rungame

--- airtest/AirtestIDE/pocotest/report/simpreport.py
+++ airtest/AirtestIDE/pocotest/report/simpreport.py
@@ -1,7 +1,5 @@
 import os.path
 import time
-from airtest.core import settings
-from airtest.report.report import simple_report
 from airtest.report.report import LogToHtml
 
 
@@ -17,9 +15,6 @@ def report():
             print("exist")
         else:
             os.mkdir(i, 777)
-    # time.sleep(20)
-    #simple_report(__file__, logpath=True, output=r"D:\testtools\testtools\airtest\AirtestIDE\pocotest\report\logfile\log.html")
-    #simple_report(__file__,logpath=True,output=r"D:\test\report02\log.html")
     h1 = LogToHtml(script_root=__file__, log_root=(r"%s" %log_path), export_dir=(r"%s" %export_report),
                    logfile=(r'%s' %logfile), lang='en', plugins=None)
     h1.report()
---
+++ airtest/AirtestIDE/pocotest/rungame.py
@@ -0,0 +1,42 @@
+import sys
+from airtest.core.api import *
+from airtest.cli.parser import cli_setup
+from airtest.core.api import *
+from report.simpreport import report
+
+
+class BingoVTestCase:
+    def __init__(self):
+        os.system("adb devices")
+        self.app_package = "com.bingo.cruise.free.best.top.game"
+        self.package_path = "D:/Downloads/BingoVoyage_dev_android_package_1613_pocotest_test_1.22.1.apk"
+        import element_path
+        self.element_path = element_path
+        self.test_log = os.path.dirname(os.path.abspath(__file__)) + "\\report"
+        self.log_path = os.path.dirname(os.path.abspath(__file__)) + "\\report\\log"
+        self.test_device = "Android://127.0.0.1:5037/emulator-5554"
+        if not cli_setup():
+            auto_setup(__file__, logdir=self.log_path,
+                       devices=[self.test_device], project_root=self.test_log)
+
+
+    def runGame(self):
+        uninstall(self.app_package)
+        time.sleep(1)
+        install(self.package_path)
+        time.sleep(1)
+        start_app(self.app_package)
+        time.sleep(10)
+        # print("start...")
+        from action import click_button
+        click_button(self.element_path.login)
+        time.sleep(1)
+        stop_app(self.app_package)
+        # generate html report
+
+    def runTest(self):
+        self.runGame()
+
+    def tearDown(self):
+        self.runTest()
+        report()
