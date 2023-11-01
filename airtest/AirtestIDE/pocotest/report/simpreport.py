ce1c29e493c0d3e0e30bc59e1dd3b471667655b2
xmznmn1983 <Simon@joycastle.mobi>
2023-11-01 15:01:01 +0800

    logpath

--- airtest/AirtestIDE/pocotest/report/simpreport.py
+++ airtest/AirtestIDE/pocotest/report/simpreport.py
@@ -1,4 +1,19 @@
+import os.path
+
 from airtest.report.report import simple_report
 
 def report():
-    simple_report(__file__)
+
+    path = "C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\airtest\\airtest\\AirtestIDE\\pocotest\\"
+    filepath = path + "filepath"
+    logpath = path + "logpath"
+    logfile = path + "logfile"
+    output = path + "output"
+    path_num = [filepath, logpath, logfile, output]
+    for i in path_num:
+        if os.path.exists(i):
+            print("exist")
+        else:
+            os.mkdir(i)
+            
+    simple_report(filepath, logpath, logfile, output)
